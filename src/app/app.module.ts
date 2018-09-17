9ea9c726893ca6dd0688f9da6ee9e5ae035110f4
Eury <32747825+AFMR@users.noreply.github.com>
2018-09-16 21:51:29 -0700

    added search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,32 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherService } from './weather/weather.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { MatInputModule, MatFormFieldModule, MatIconModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    CitySearchComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  exports:[
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
